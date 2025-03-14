b5ad561f59cab1fc0f6bb7ccdb15f3aa349b00c7
Rashmikanth18 <130748001+Rashmikanth18@users.noreply.github.com>
2025-03-14 11:58:01 +0530

    Initial commit

---
+++ Face.java
@@ -0,0 +1,11 @@
+class Face {
+    String shape;
+    int age;
+    boolean hasBeard;
+
+    Face(String shape, int age, boolean hasBeard) {
+        this.shape = shape;
+        this.age = age;
+        this.hasBeard = hasBeard;
+    }
+}
