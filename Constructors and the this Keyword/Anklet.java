6469b4f17e7c0ea2098eb19b095049a0d5fb7c56
Rashmikanth18 <130748001+Rashmikanth18@users.noreply.github.com>
2025-03-21 12:05:20 +0530

    Added Java files to Constructors and the this Keyword folder

---
+++ Constructors and the this Keyword/Anklet.java
@@ -0,0 +1,11 @@
+class Anklet {
+    String material;
+    int length;
+    boolean adjustable;
+
+    Anklet(String material, int length, boolean adjustable) {
+        this.material = material;
+        this.length = length;
+        this.adjustable = adjustable;
+    }
+}
---
+++ Constructors and the this Keyword/Magnet.java
@@ -0,0 +1,11 @@
+class Magnet {
+    String type;
+    int strength;
+    boolean permanent;
+
+    Magnet(String type, int strength, boolean permanent) {
+        this.type = type;
+        this.strength = strength;
+        this.permanent = permanent;
+    }
+}
