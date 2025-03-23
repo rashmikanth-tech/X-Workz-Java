be4cc2ca5e884adb8200b37abd1126ff1fa25d23
Rashmikanth18 <130748001+Rashmikanth18@users.noreply.github.com>
2025-03-23 22:01:15 +0530

    Added Method Overloading folder

---
+++ Method Overloading/Rashmikanth.java
@@ -0,0 +1,30 @@
+class Rashmikanth{
+	public static void myAge(int age){
+		System.out.println("my age :"+age);
+	}
+		
+		public static void myHeight(double height){
+			System.out.println("My Height is :"+height);
+		}
+			
+			public static void bloodGroup(String bloodType){
+				System.out.println("My bloodGroup is : " +bloodType);
+			}				
+				
+				public static void color(String colorType, short shoesSize){
+					System.out.println("Skin Type :"+colorType+ "\nShoes Size :"+shoesSize);
+				}
+					
+					public static void work(String intern, int km){
+						System.out.println("Intern at :"+intern+"\nTravel Distance :"+km);
+					}
+						
+						public static void myAddress(String place,int pincode, long mobileNo){
+							System.out.println("address is :"+place+"\npincode :"+pincode+"\nMobile Number: "+mobileNo);
+						}
+							
+							public static void letExperiment(byte Singer,short easy,int Marvel,float Boys,double pin,long mob,String Rash,boolean yes){
+							System.out.println("hi:"+Singer+"\nHeight: "+easy+"\nFan :"+Marvel+"\nin points: "+Boys+"\nCode :"+pin+"\n mobile"+mob+"\n Me : "+Rash+"\nTrue of False :"+yes);
+							}
+							
+}
